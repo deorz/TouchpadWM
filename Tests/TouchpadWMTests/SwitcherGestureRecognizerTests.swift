8a446c46ea33e7b53e7f9cda3b9a07940d33516d
Denis Orzhenovskii <thedamnation1044@yandex.ru>
2026-08-25 18:39:56 +0300

    feat: translate three-finger gestures into switcher commands

---
+++ Tests/TouchpadWMTests/SwitcherGestureRecognizerTests.swift
@@ -0,0 +1,68 @@
+import TouchpadWMSpike
+import XCTest
+
+@testable import TouchpadWM
+
+final class SwitcherGestureRecognizerTests: XCTestCase {
+  func testThreeFingerContactImmediatelyOpensTheSwitcher() {
+    var recognizer = SwitcherGestureRecognizer()
+
+    XCTAssertEqual(recognizer.consume(frame(y: 0.5)), [.openSwitcher])
+  }
+
+  func testCrossingOneThresholdUpwardMovesToThePreviousRow() {
+    var recognizer = SwitcherGestureRecognizer()
+    _ = recognizer.consume(frame(y: 0.5))
+
+    let commands = recognizer.consume(
+      frame(y: 0.5 + SwitcherGestureRecognizer.rowSelectionThreshold))
+
+    XCTAssertEqual(commands, [.moveSwitcherSelection(.previous)])
+  }
+
+  func testMovingBackBelowTheThresholdMovesToTheNextRow() {
+    var recognizer = SwitcherGestureRecognizer()
+    _ = recognizer.consume(frame(y: 0.5))
+    _ = recognizer.consume(frame(y: 0.5 + SwitcherGestureRecognizer.rowSelectionThreshold))
+
+    let commands = recognizer.consume(frame(y: 0.5))
+
+    XCTAssertEqual(commands, [.moveSwitcherSelection(.next)])
+  }
+
+  func testSubThresholdMovementProducesNoCommand() {
+    var recognizer = SwitcherGestureRecognizer()
+    _ = recognizer.consume(frame(y: 0.5))
+
+    let commands = recognizer.consume(
+      frame(y: 0.5 + SwitcherGestureRecognizer.rowSelectionThreshold / 2))
+
+    XCTAssertEqual(commands, [])
+  }
+
+  func testCrossingTwoThresholdsInOneFrameEmitsTwoMoveCommands() {
+    var recognizer = SwitcherGestureRecognizer()
+    _ = recognizer.consume(frame(y: 0.5))
+
+    let commands = recognizer.consume(
+      frame(y: 0.5 + 2 * SwitcherGestureRecognizer.rowSelectionThreshold))
+
+    XCTAssertEqual(commands, [.moveSwitcherSelection(.previous), .moveSwitcherSelection(.previous)])
+  }
+
+  func testLiftingAllFingersActivatesTheSelection() {
+    var recognizer = SwitcherGestureRecognizer()
+    _ = recognizer.consume(frame(y: 0.5))
+
+    let commands = recognizer.consume(frame(ids: [1, 2], y: 0.5))
+
+    XCTAssertEqual(commands, [.activateSwitcherSelection])
+  }
+
+  private func frame(ids: [Int32] = [1, 2, 3], y: Float) -> TouchFrame {
+    TouchFrame(
+      contacts: zip(ids, [Float](arrayLiteral: 0.2, 0.5, 0.8)).map { id, x in
+        TouchContact(id: id, x: x, y: y)
+      })
+  }
+}
